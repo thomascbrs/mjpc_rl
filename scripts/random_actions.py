5eecf399932d4888cffa07f779c4d1d5862c76bb
thomasCbrs <thomas.corberes@gmail.com>
2024-03-19 10:43:41 +0000

    Add random action script

---
+++ scripts/random_actions.py
@@ -0,0 +1,28 @@
+from build_release.libmjpc_rl_pywrap import MujocoSimulator,loadData
+
+import numpy as np
+from time import sleep
+from time import perf_counter as clock
+
+from envs.BaseEnv import BaseEnv
+
+# from gymnasium import spaces
+# from gymnasium.wrappers import FlattenObservation, RescaleAction
+
+if __name__ == "__main__":
+    env = BaseEnv(render_mode = "human")
+    # observation, info = env.reset()
+    for k in range(500):
+        actions = env.action_space.sample()
+        observation, reward, terminated, truncated, info = env.step(actions)
+        # print(observation["ori_ref"])
+        if terminated:
+            print("Terminated due to collision")
+            env.reset()
+        if truncated:
+            print("Truncated due to end time reached.")
+            env.reset()
+
+
+    # sleep(2.)
+    # observation, reward, terminated, truncated, info = env.step(action)
